b6d5ea8da75b976a0ce98c15db6d7c538a9f1d51
Aaryaman Gupta <50963421+aarya7@users.noreply.github.com>
2020-06-10 21:21:44 +0530

    Add files via upload

---
+++ database_chatbot/queries/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User                               
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from queries.serializers import  query1Serializer # Our Serializer Classes
+from rest_framework.decorators import api_view
+from nlp_module import respond
+import response_generation
+import json
+
+@api_view(['GET', 'POST'])
+@csrf_exempt                         
+def message_list(request, sender=None, receiver=None):
+    """
+    List all required messages, or create a new message.
+    """
+    if request.method == 'POST':
+        var=json.dumps(request)
+        input_question=var["messages"]
+        nlp_out = respond(input_question)
+        final_out = response_generation(nlp_out)
+        body = {"message": final_out}
+        return JsonResponse(body, status=200)
