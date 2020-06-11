174d3a0bc7e22a93395da4438e4acc50ece0cf2d
rishabh-11 <66425093+rishabh-11@users.noreply.github.com>
2020-06-11 16:51:16 +0530

    updated the file with try and except blocks

--- database_chatbot/queries/response_generator_1.py
+++ database_chatbot/queries/response_generator_1.py
@@ -1,10 +1,13 @@
-
 def response_generation(s):
     from django.db import connection
     with connection.cursor() as cursor:
-        sql_query=("SELECT response from queries_query WHERE intent=%s")
+        sql_query=("SELECT val from test WHERE id=%s")
         cursor.execute(sql_query,[s])
         for x in cursor:
             s1=x
-        s2=''.join(s1)
+        try:
+        	s2=''.join(s1)
+        except:
+        	s2="Sorry, cannot understand the question from provided information."
+        	return s2
     return s2
