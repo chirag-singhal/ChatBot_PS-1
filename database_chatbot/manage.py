564a4b319b682ad6bda268da6121cebe47c604f8
Aaryaman Gupta <50963421+aarya7@users.noreply.github.com>
2020-06-20 15:11:36 +0530

    Update manage.py

--- database_chatbot/manage.py
+++ database_chatbot/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 '''
+Automatically created for each Django project.
 Sets the DJANGO_SETTINGS_MODULE environment variable so that it
 points to your project’s settings.py file.
 
