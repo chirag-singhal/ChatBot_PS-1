1ddcc4ab9e0aa97eff8929d909f18b25df3fc88f
Aaryaman Gupta <50963421+aarya7@users.noreply.github.com>
2020-06-21 14:06:53 +0530

    Update models.py

--- database_chatbot/queries/models.py
+++ database_chatbot/queries/models.py
@@ -1,6 +1,16 @@
+'''
+A model is the single, definitive source of information about your data. 
+It contains the essential fields and behaviors of the data you’re storing. 
+Generally, each model maps to a single database table.
+For more info, refer to https://docs.djangoproject.com/en/3.0/topics/db/models/
+
+'''
+
 from django.db import models
 
 class Query(models.Model):
+    '''
+    '''
     intent = models.CharField(max_length=2000)
     response = models.CharField(max_length=5530)
     
