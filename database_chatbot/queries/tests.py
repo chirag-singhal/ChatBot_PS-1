31690c033d1766759ec604961c51bd070d1942c6
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2020-06-22 00:01:37 +0530

    added documentation

--- database_chatbot/queries/tests.py
+++ database_chatbot/queries/tests.py
@@ -1,3 +1,9 @@
+"""Testing a website is a complex task, because it is made of several layers of logic – from HTTP-level request handling,
+queries models, to form validation and processing, and template rendering. 
+Django provides a test framework with a small hierarchy of classes that build on the Python standard unittest library. 
+For more info visit https://docs.djangoproject.com/en/3.0/topics/testing/#:~:text=The%20preferred%20way%20to%20write,for%20that%20kind%20of%20integration.
+    
+    """
 from django.test import TestCase
 
 # Create your tests here.
