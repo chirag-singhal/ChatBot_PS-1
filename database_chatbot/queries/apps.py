858bb36dc6e696531bb1bb1e37b3223fbda6c62e
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2020-06-22 00:00:02 +0530

    added documentation

--- database_chatbot/queries/apps.py
+++ database_chatbot/queries/apps.py
@@ -1,3 +1,9 @@
+"""
+This file is created to help the user include any application configuration for the app.
+Using this, you can configure some of the attributes of the application. 
+For more info visit https://docs.djangoproject.com/en/3.0/ref/applications/
+
+"""
 from django.apps import AppConfig
 
 
