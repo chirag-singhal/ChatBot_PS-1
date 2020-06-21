2d9c8d829e5b6f101b807e15e86e9c93edb9ae5f
Aaryaman Gupta <50963421+aarya7@users.noreply.github.com>
2020-06-21 14:17:11 +0530

    Added documentation

--- database_chatbot/queries/serializers.py
+++ database_chatbot/queries/serializers.py
@@ -1,7 +1,18 @@
+'''
+Serializers allow complex data such as querysets and model instances 
+to be converted to native Python datatypes that can then be easily rendered 
+into JSON, XML or other content types. Serializers also provide deserialization, 
+allowing parsed data to be converted back into complex types, 
+after first validating the incoming data.
+'''
+
 from rest_framework import serializers
 from .models import Query
 
 class query1Serializer(serializers.ModelSerializer):
+    '''
+    Class for serializing user query
+    '''
     class Meta:
         model = Query
         fields = '__all__'
