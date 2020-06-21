c5235b14ec427c954c4397640d600e7277db4625
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2020-06-21 23:58:29 +0530

    added documentation

--- database_chatbot/database_chatbot/__init__.py
+++ database_chatbot/database_chatbot/__init__.py
@@ -1,3 +1,11 @@
+ """The __init__.py file makes Python treat directories containing it as modules. Furthermore, 
+ this is the first file to be loaded in a module,
+ so it can be used to execute code that has to be run each time a module is loaded,
+ or specify the submodules to be exported.
+
+ PyMySQL is a pure-Python MySQL client library.
+
+ """
 import pymysql
 pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
