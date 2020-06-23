7db911e88c5cc571ccb657c47302f94ce91187ce
rishabh-11 <66425093+rishabh-11@users.noreply.github.com>
2020-06-23 18:20:04 +0530

    updated the file
    Database Team, just check whether the column and table names are correct or not.

--- database_chatbot/queries/response_generator_1.py
+++ database_chatbot/queries/response_generator_1.py
@@ -1,13 +1,16 @@
-def response_generation(s):
+def response_generation(s,input_question):
     from django.db import connection
     with connection.cursor() as cursor:
+        if s=="-1":
+            try:
+                cursor.execute("INSERT INTO table2(unanswered_query) VALUES(%s)",[input_question])
+            except:
+                pass
+            s2="Sorry, cannot understand the question from provided information."
+            return s2
         sql_query=("SELECT response from queries_query WHERE intent=%s")
         cursor.execute(sql_query,[s])
         for x in cursor:
             s1=x
-        try:
-        	s2=''.join(s1)
-        except:
-        	s2="Sorry, cannot understand the question from provided information."
-        	return s2
+        s2=''.join(s1)
     return s2
