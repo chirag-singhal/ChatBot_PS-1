0fe351328bd22319a9ee4713e5408960214e6416
rishabh-11 <66425093+rishabh-11@users.noreply.github.com>
2020-06-23 19:28:48 +0530

    Update response_generator_1.py

--- database_chatbot/queries/response_generator_1.py
+++ database_chatbot/queries/response_generator_1.py
@@ -2,15 +2,21 @@ def response_generation(s,input_question):
     from django.db import connection
     with connection.cursor() as cursor:
         if s==-1:
+            cursor.execute("SELECT * FROM table2")
+            for x in cursor:
+                if x[0]==input_question:
+                    s2="Sorry, cannot understand the question from provided information."
+                    return s2
             try:
                 cursor.execute("INSERT INTO table2(unanswered_query) VALUES(%s)",[input_question])
             except:
                 pass
             s2="Sorry, cannot understand the question from provided information."
             return s2
-        sql_query=("SELECT response from queries_query WHERE intent=%s")
+        sql_query=("SELECT val from test WHERE id=%s")
         cursor.execute(sql_query,[s])
         for x in cursor:
+            print(x)
             s1=x
         s2=''.join(s1)
     return s2
