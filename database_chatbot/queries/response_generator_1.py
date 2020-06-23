1cbe1313b07d8bf5bf5d873fe1b8cdd40170ddbc
rishabh-11 <66425093+rishabh-11@users.noreply.github.com>
2020-06-23 21:47:46 +0530

    updated with correct column and table names

--- database_chatbot/queries/response_generator_1.py
+++ database_chatbot/queries/response_generator_1.py
@@ -8,12 +8,12 @@ def response_generation(s,input_question):
                     s2="Sorry, cannot understand the question from provided information."
                     return s2
             try:
-                cursor.execute("INSERT INTO table2(unanswered_query) VALUES(%s)",[input_question])
+                cursor.execute("INSERT INTO Unanswered_query(unanswered_query) VALUES(%s)",[input_question])
             except:
                 pass
             s2="Sorry, cannot understand the question from provided information."
             return s2
-        sql_query=("SELECT val from test WHERE id=%s")
+        sql_query=("SELECT response from Query WHERE intent=%s")
         cursor.execute(sql_query,[s])
         for x in cursor:
             print(x)
